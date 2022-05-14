1d9569e6f52c20575a0094ed3df06ad580436f24
Adrian Fraire-Vasquez <Adfrva@gmail.com>
2022-05-13 21:26:02 -0600

    troubleshooting

--- pwGenerator.py
+++ pwGenerator.py
@@ -2,16 +2,23 @@ import random
 
 niceWordFile = open("positive-words.txt")
 niceWords = niceWordFile.read()
+randNum = random.randint(0,25)
 
 print("Welcome to the password generator\n")
-print("Do you want a new password? Type Y or N")
-userAnswer = input()
-generatedPW = ""
 
-if userAnswer == "Y" or userAnswer == "y":
-    generatedPW = generatedPW + random.choice(niceWords)
-    print("Heres your password: ", generatedPW)
-elif userAnswer == "N" or userAnswer == "n":
-    print("goodbye")
-else:
-    print("Wrong answer")
+userContinue = 1
+
+while userContinue==1:
+    print("Do you want a new password? Type Y or N")
+    userAnswer = input()
+    generatedPW = ""
+    if userAnswer == "Y" or userAnswer == "y":
+        generatedPW = niceWords[randNum]
+        print("Heres your password: ", generatedPW)
+        generatedPW = ""
+    elif userAnswer == "N" or userAnswer == "n":
+        userContinue == 0
+        print("goodbye")
+        continue
+    else:
+        print("Wrong answer")
